e85f1f173b2b03066f6340ec9e5e3d53cf6704dc
Maya <mayalauren.siu@gmail.com>
2022-07-18 16:20:05 -0700

    Finished Iteration 1

---
+++ toDoList2022/AddToDoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddToDoViewController.swift
+//  toDoList2022
+//
+//  Created by Scholar on 7/18/22.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    var previousVC = ToDoTableViewController()
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var titleTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+      let toDo = ToDo()
+    
+      if let titleText = titleTextField.text {
+        toDo.name = titleText
+        toDo.important = importantSwitch.isOn
+      }
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- toDoList2022/ToDoTableViewController.swift
+++ toDoList2022/ToDoTableViewController.swift
@@ -58,8 +58,26 @@ class ToDoTableViewController: UITableViewController {
             }//creating the cell that we print out
         return cell
     }
-        
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let toDo = toDos[indexPath.row]
+        performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddToDoViewController{
+            addVC.previousVC = self
+        }
+        if let completeVC = segue.destination as? CompleteToDoViewController{
+            if let toDo = sender as? ToDo{
+                completeVC.selectedToDo = toDo
+                completeVC.previousVC = self
+        }
+        }
+    }
+    
+    
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
